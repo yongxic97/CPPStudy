8ac03b6e6dd4ac200c614f0c328747dc49f29bed
Cao Yongxi <47467702+cyxsustech@users.noreply.github.com>
2022-02-23 02:26:51 +0800

    添加项目文件。

---
+++ CPP_namespace/CPP_namespace.cpp
@@ -0,0 +1,16 @@
+﻿#include <iostream>
+
+using namespace std;
+
+namespace first_space {
+	void f() {
+		cout << "first space function" << endl;
+	}
+}
+
+namespace second_space {
+	void f() {
+		cout << "second space function" << endl;
+	}
+}
+
